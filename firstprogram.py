936f36252a14acaa03933f7d8a6d444979e417a7
Amos Omariba <amomariba@gmail.com>
2024-11-24 23:56:09 +0300

    Create first program

---
+++ firstprogram.py
@@ -0,0 +1,17 @@
+# This programs says hello and ask for my name.
+
+print("Hello world!!")
+print("What is your name?")
+myName = input()
+print(f"It's good to meet you, {myName}.")
+print(f"The length of your name is: {len(myName)}")
+# print(len(myName))
+print("What is your age?")
+myAge = input()
+print(f"You will be {int(myAge)+1} in a year")
+print('Which primary school did you go?')
+myPrimary=input()
+print('That is a good school')
+print('Which year did you sit for your K.C.P.E ?')
+sitKcpe=input()
+print(f"Thank you {myName} for answering our questions!")
