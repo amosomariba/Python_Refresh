d841d5149c8e279d796d42ee75ac26a8a956f9f9
Amos Omariba <amomariba@gmail.com>
2024-11-25 17:10:32 +0300

    Add elif statement on controlflow.py file

---
+++ blocks.py
@@ -0,0 +1,11 @@
+name = "Mary"
+password = "wordfish"
+
+#print('Enter your name,')
+
+if name == "Mary":
+    print("Hello Mary")
+if password == "swordfish":
+    print("Access granted.")
+else:
+    print("Wrong password.")
