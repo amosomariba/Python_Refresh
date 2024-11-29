84371da7fcb255bc2c2d86434183b0e0b9049215
Amos Omariba <amomariba@gmail.com>
2024-11-29 11:06:04 +0300

    Create a file to calculate area of a rectangle

---
+++ functions3.py
@@ -0,0 +1,10 @@
+def area_of_rectangle(a, b):
+    area = a * b
+    return area
+
+
+rectangle_one = area_of_rectangle(2, 5)
+rectangle_two = area_of_rectangle(2, 10)
+
+print(f"The area of the first rectangle is {rectangle_one}")
+print(f"The area of the second rectangle is {rectangle_two}")
