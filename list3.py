0c085be51b5ef0cbfbda87d76b8b3291e0046712
Amos Omariba <amomariba@gmail.com>
2024-12-02 15:01:35 +0300

    Create a file for adding elements to a python list

---
+++ list3.py
@@ -0,0 +1,9 @@
+# Add Elements to a Python List
+fruits = ["apple", "banana", "orange"]
+print("Original List:", fruits)
+
+fruits.append(
+    "cherry"
+)  # We use the append() method to add an element to the end of a Python list
+
+print("Updated List:", fruits)
