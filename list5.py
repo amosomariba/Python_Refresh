d7fbbaed41c1d83fa3792beed42562f75d5d10c8
Amos Omariba <amomariba@gmail.com>
2024-12-02 15:05:19 +0300

    Create a file that can change items in the list

---
+++ list5.py
@@ -0,0 +1,8 @@
+# We can change the items of a list by assigning new values using the = operator
+colors = ["Red", "Black", "Green"]
+print("Original List:", colors)
+
+# changing the third item to 'Blue'
+colors[2] = "Blue"
+
+print("Updated List:", colors)
