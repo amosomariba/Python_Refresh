b3275c92df43c36cc80c1f324e9666892c41b903
Amos Omariba <amomariba@gmail.com>
2024-12-02 15:11:29 +0300

    Create a file that uses for loop in a list

---
+++ list8.py
@@ -0,0 +1,7 @@
+# We can use a for loop to iterate over the elements of a list
+fruits = ["apple", "banana", "orange"]
+length_of_fruits = len(fruits)
+for fruit in fruits:
+    print(fruit)
+
+print(f'The length of the fruits list is:{length_of_fruits}')
