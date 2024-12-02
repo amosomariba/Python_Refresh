6787086611e36abf49334fa6176ca9f4f159d29c
Amos Omariba <amomariba@gmail.com>
2024-12-02 15:03:44 +0300

    Create a file that has insert method

---
+++ list4.py
@@ -0,0 +1,7 @@
+# The insert() method adds an element at the specified index
+fruits = ["apple", "banana", "orange"]
+print("Original List:", fruits)
+
+fruits.insert(2, "cherry")  # insert 'cherry' at index 2
+
+print("Updated List:", fruits)
