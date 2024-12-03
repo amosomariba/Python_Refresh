0f5a125cc3a3a6f17fbd07667bbd46a6ba909c8f
Amos Omariba <amomariba@gmail.com>
2024-12-03 17:26:05 +0300

    Create a multiple objects of python class

--- oop_class2.py
+++ oop_class2.py
@@ -8,9 +8,9 @@ employee1 = Employee()
 employee2 = Employee()
 
 employee1.employeeID = 10001
-employee1.name='Caren'
+employee1.name = "Caren"
 print(f"Name:{employee1.name}, Employee ID:{employee1.employeeID}")
 
 employee2.employeeID = 10002
-employee2.name='Suchi'
+employee2.name = "Suchi"
 print(f"Name:{employee2.name}, Employee ID:{employee2.employeeID}")
